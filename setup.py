5ea92ddc9c819ae22ab9c90009ea597d7b7dc810
oliyyaa <olga.chaganova1@gmail.com>
2022-08-14 19:18:33 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hw1_service',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Service for multilabel classification (hw1)',
+    author='Olga Chaganova',
+    license='',
+)
