a891a1030647b7b08a9be18cada6de8121271831
oliyyaa <olga.chaganova1@gmail.com>
2022-09-12 16:52:53 +0300

    del

--- deploy/ansible/templates/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ue
-
-docker image prune -af
-docker container prune -af
--- deploy/ansible/templates/destroy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ue
-
-CONTAINER_NAME={{ container_name }}
-
-docker stop ${CONTAINER_NAME} || true
-
-docker rm -f ${CONTAINER_NAME} || true
